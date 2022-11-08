a57afcaa4717d894ecf642ebd17f1f1405485ec2
Dan Barr <dan.barr@outlook.com>
2022-11-08 17:07:56 -0500

    Your basic hello world

---
+++ main.cbl
@@ -0,0 +1,13 @@
+       >>SOURCE FORMAT FREE
+identification division.
+program-id. HELLOWORLD.
+
+environment division.
+
+data division.
+working-storage section.
+01 blarg PIC X(12) value 'BELLO WORLD'.
+
+procedure division.
+display blarg.
+stop run.
