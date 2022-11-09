ddbac31c5c91f7589800f06127e79436adb88e82
Dan Barr <dan.barr@outlook.com>
2022-11-08 22:03:08 -0500

    so far, it compiles

--- main.cbl
+++ main.cbl
@@ -6,8 +6,14 @@ environment division.
 
 data division.
 working-storage section.
-01 blarg PIC X(12) value 'BELLO WORLD'.
+01 xmtr-power      pic 9(4)      value 1000.
+01 feedline-length pic 9(2)      value 73.
+01 duty-cycle      pic 9(1).9(2) value .5.
+01 per-30          pic 9(1).9(2) value .5.
+01 cablevalues.
+       02 k1 pic 9(1).9(6) value .122290.
+       02 k2 pic 9(1).9(6) value .000260.
 
 procedure division.
-display blarg.
+display duty-cycle.
 stop run.
